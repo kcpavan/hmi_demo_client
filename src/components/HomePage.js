11d1ef48ea357f4d761a9c63100ebaa731101b65
Pavan Kuamr <kcpavan@me.com>
2019-09-10 18:57:46 +0530

    show users in home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,29 +2,53 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { userActions } from '../_actions';
+import { connect } from 'react-redux';
 
+class HomePage extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(userActions.getAll());
+  }
+  render() {
+    const { user, users } = this.props;
 
+    return (<React.Fragment>
 
-const HomePage = () => (
-  <React.Fragment>
+      <Grid container spacing={24}>
+        <Grid item xs={12} style={{ marginTop: 10 }}>
+          <Paper className="homepagepaper">
+            <Typography variant="body1" gutterBottom align="center">
+              You're logged in with React & JWT!!
+          </Typography>
 
-    <Grid container spacing={24}>
-      <Grid item xs={12} style={{ marginTop: 10 }}>
-        <Paper className="homepagepaper">
-          <Typography variant="body1" gutterBottom align="center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </Typography>
-
-        </Paper>
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <p>You're logged in with React & JWT!!</p>
+          <h3>Users from secure api end point:</h3>
+          {users.loading && <em>Loading users...</em>}
+          {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+          {users.items &&
+            <ul>
+              {users.items.map((user, index) =>
+                <li key={user.id}>
+                  {user.name + ' ' + user.email}
+                </li>
+              )}
+            </ul>
+          }
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        test
-      </Grid>
-      <Grid item xs={6}>
-        test
-      </Grid>
-    </Grid>
-  </React.Fragment>
-);
-
-export default HomePage;
+    </React.Fragment>);
+  }
+}
+function mapStateToProps(state) {
+  const { users, authentication } = state;
+  const { user } = authentication;
+  return {
+    user,
+    users
+  };
+}
+const connectedHomePage = connect(mapStateToProps)(HomePage);
+export { connectedHomePage as HomePage };
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import HomePage from '../components/HomePage';
+import { HomePage } from '../components/HomePage';
 import NotFoundPage from '../components/NotFoundPage';
 
 import PublicRoute from './PublicRouter';
